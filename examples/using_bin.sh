6a091962fc413b3fd9d45c2cc81859be1a78f191
Aaron Sheriff <aaron.sheriff@gmail.com>
2011-06-12 21:37:00 -0500

    Added example of using awk to write the questions and answers files from the cli

--- examples/using_bin.sh
+++ examples/using_bin.sh
@@ -14,3 +14,10 @@
 # Set field separator in output
 #
 ./bin/unique_substring_finder -F: spec/data/test_words
+
+#
+# Using awk to write words and substrings to separate files
+#
+./bin/unique_substring_finder spec/data/dolan_words |
+  sort |
+  awk '{ print $1 > "spec/output/questions"; print $2 > "spec/output/answers" }'
